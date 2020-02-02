2a94cd9faaccda34d2c5572948e0f8297942e2a5
benkissi <benkissi111@gmail.com>
2020-02-02 19:56:50 +0000

    bg(): use https for mocky call

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const endpoints = {
-    products: `http://www.mocky.io/v2/5c3e15e63500006e003e9795`
+    products: `https://www.mocky.io/v2/5c3e15e63500006e003e9795`
 }
 export const getProducts = async () => {
     const products = await axios.get(endpoints.products)
